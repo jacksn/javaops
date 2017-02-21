ecc1b10389db089dd681a3b84f62d4935a77cd27
Grigory Kislin <gkislin@yandex.ru>
2017-02-21 22:25:08 +0300

    Update jr info

--- src/test/java/ru/javaops/service/MailServiceTest.java
+++ src/test/java/ru/javaops/service/MailServiceTest.java
@@ -1,11 +1,8 @@
 package ru.javaops.service;
 
-import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javaops.ApplicationAbstractTest;
-import ru.javaops.UserTestData;
-import ru.javaops.util.Util;
 
 /**
  * GKislin
@@ -17,12 +14,7 @@ public class MailServiceTest extends ApplicationAbstractTest {
     private MailService mailService;
 
     @Test
-    public void testGetContent() throws Exception {
-        String content = mailService.getContent("basejava_register",
-                ImmutableMap.of("user", UserTestData.USER, "template", "topjava_register", "participation", "REGISTRATION", "result", "OK", "activationKey", "activationKey"));
-        System.out.println("\n+++++");
-        System.out.println(Util.getTitle(content));
-        System.out.println("+++++");
-        System.out.println(content);
+    public void testSendMail() throws Exception {
+        System.out.println(mailService.sendTest("jr_confirm"));
     }
 }
